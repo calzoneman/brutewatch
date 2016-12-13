8a53897c4fca22ecd59e88682879c234cfd24c1b
Calvin Montgomery <calzoneman@gmail.com>
2016-12-12 18:28:08 -0800

    Add SSH daemon

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ const ConsoleLogger = require('./lib/consolelogger');
 ConsoleLogger.initialize(LogLevel.INFO);
 
 const TelnetServer = require('./lib/telnet/telnet-server');
+const SSHServer = require('./lib/ssh/ssh-server');
 const Webserver = require('./lib/web/webserver');
 const WebsocketController = require('./lib/web/ws-controller');
 const EventSender = require('./lib/event-sender');
 const config = require('./config');
 
 const eventSender = new EventSender();
-new TelnetServer({
-    eventSender: eventSender
-}).listen(config.telnet.port, config.telnet.host);
 
-new Webserver({
-    wsController: new WebsocketController({ eventEmitter: eventSender })
-}).listen(config.http.port, config.http.host);
+if (config.telnet.enabled) {
+    new TelnetServer({
+        eventSender: eventSender
+    }).listen(config.telnet.port, config.telnet.host);
+}
+
+if (config.ssh.enabled) {
+    new SSHServer({
+        eventSender: eventSender,
+        hostKey: config.ssh.hostKey
+    }).listen(config.ssh.port, config.ssh.host);
+}
+
+if (config.http.enabled) {
+    const wsController = new WebsocketController({
+        eventEmitter: eventSender
+    });
+    new Webserver({
+        wsController: wsController
+    }).listen(config.http.port, config.http.host);
+}
